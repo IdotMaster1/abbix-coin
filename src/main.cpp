acc6b2321e15ab32433dc000cefedc109fcaa436
Abb1x <loicgre@gmail.com>
2020-12-04 16:04:37 -0500

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,16 @@
 
 int main()
 {
-    Blockchain bChain = Blockchain();
+    Blockchain block_chain = Blockchain();
 
-    std::cout << "Mining block 1..." << std::endl;
-    bChain.add_block(Block(1, "Block 1 Data"));
+    std::cout << "Mining block 1" << std::endl;
+    block_chain.add_block(Block(1, "Block 1 Data"));
 
-    std::cout << "Mining block 2..." << std::endl;
-    bChain.add_block(Block(2, "Block 2 Data"));
+    std::cout << "Mining block 2" << std::endl;
+    block_chain.add_block(Block(2, "Block 2 Data"));
 
-    std::cout << "Mining block 3..." << std::endl;
-    bChain.add_block(Block(3, "Block 3 Data"));
+    std::cout << "Mining block 3" << std::endl;
+    block_chain.add_block(Block(3, "Block 3 Data"));
 
     return 0;
 }
